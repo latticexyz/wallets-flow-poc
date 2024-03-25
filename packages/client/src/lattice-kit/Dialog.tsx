1d7a8ddc8cfdfbaf89f25b69149953fcc527911c
karooolis <hello@karolisram.com>
2024-03-25 18:12:55 +0000

    add onboarding dialog skeleton

--- packages/client/src/lattice-kit/Dialog.tsx
+++ packages/client/src/lattice-kit/Dialog.tsx
@@ -1,20 +1,17 @@
 import { useEffect, useState } from "react";
-import { Flex, Text, Button, Dialog, TextField } from "@radix-ui/themes";
+import { Box, Flex, Text, Button, Dialog, Tabs } from "@radix-ui/themes";
 import { useAccount } from "wagmi";
 
+type FlowState = "signer" | "balance" | "delegate" | "play";
+
 const LatticeKitDialog = () => {
+  const [activeTab, setActiveTab] = useState<FlowState>("signer");
   const [open, setOpen] = useState(false);
   const [shown, setShown] = useState(false);
   const account = useAccount();
   const isConnected = account?.isConnected;
 
-  console.log(account);
-  console.log(account?.address);
-  console.log(account?.isConnected);
-
   useEffect(() => {
-    console.log("isConnected", isConnected);
-
     if (isConnected && !shown) {
       setOpen(true);
       setShown(true);
@@ -24,31 +21,125 @@ const LatticeKitDialog = () => {
   return (
     <Dialog.Root open={open}>
       <Dialog.Content maxWidth="450px">
-        <Dialog.Title>Generate app-signer</Dialog.Title>
-        <Dialog.Description size="2" mb="4">
-          Generate app-signer description
-        </Dialog.Description>
+        <Tabs.Root value={activeTab}>
+          <Tabs.Content value="signer">
+            <Dialog.Title>Generate app-signer</Dialog.Title>
+            <Dialog.Description size="2" mb="4">
+              Generate app-signer description
+            </Dialog.Description>
 
-        <Flex direction="column" gap="3">
-          More context ..
-        </Flex>
+            <Text>More context ..</Text>
 
-        <Flex gap="3" mt="4" justify="end">
-          <Dialog.Close>
+            <Flex gap="3" mt="4" justify="end">
+              <Dialog.Close>
+                <Button
+                  variant="soft"
+                  color="gray"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Dialog.Close>
+              <Dialog.Close>
+                <Button
+                  onClick={() => {
+                    setActiveTab("balance");
+                  }}
+                >
+                  Generate signer
+                </Button>
+              </Dialog.Close>
+            </Flex>
+          </Tabs.Content>
+
+          <Tabs.Content value="balance">
+            <Dialog.Title>Fund Redstone Balance</Dialog.Title>
+            <Dialog.Description size="2" mb="4">
+              Fund Redstone Balance description
+            </Dialog.Description>
+
+            <Text>More context ..</Text>
+
             <Button
-              variant="soft"
-              color="gray"
+              style={{ width: "100%", marginTop: "15px" }}
               onClick={() => {
-                setOpen(false);
+                setActiveTab("delegate");
               }}
             >
-              Cancel
+              Relay.link
             </Button>
-          </Dialog.Close>
-          <Dialog.Close>
-            <Button>Generate signer</Button>
-          </Dialog.Close>
-        </Flex>
+            <Button
+              style={{ width: "100%", marginTop: "15px" }}
+              onClick={() => {
+                setActiveTab("delegate");
+              }}
+            >
+              Redstone ETH
+            </Button>
+          </Tabs.Content>
+
+          <Tabs.Content value="delegate">
+            <Dialog.Title>Delegation</Dialog.Title>
+            <Dialog.Description size="2" mb="4">
+              Delegation description
+            </Dialog.Description>
+
+            <Flex gap="3" mt="4" justify="end">
+              <Dialog.Close>
+                <Button
+                  variant="soft"
+                  color="gray"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Dialog.Close>
+              <Dialog.Close>
+                <Button
+                  onClick={() => {
+                    setActiveTab("play");
+                  }}
+                >
+                  Set up delegation
+                </Button>
+              </Dialog.Close>
+            </Flex>
+          </Tabs.Content>
+
+          <Tabs.Content value="play">
+            <Dialog.Title>You&apos;re all set up!</Dialog.Title>
+            <Dialog.Description size="2" mb="4">
+              Nothing more to do, let&apos;s play!
+            </Dialog.Description>
+
+            <Flex gap="3" mt="4" justify="end">
+              <Dialog.Close>
+                <Button
+                  variant="soft"
+                  color="gray"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  Close
+                </Button>
+              </Dialog.Close>
+              <Dialog.Close>
+                <Button
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  Let&apos;s play
+                </Button>
+              </Dialog.Close>
+            </Flex>
+          </Tabs.Content>
+        </Tabs.Root>
       </Dialog.Content>
     </Dialog.Root>
   );
