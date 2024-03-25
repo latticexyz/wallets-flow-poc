878387c39935fd7dc16e37460252e139eb7c2310
karooolis <hello@karolisram.com>
2024-03-25 16:18:45 +0000

    add RainbowKit connection

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,34 +1,36 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { setup } from "./mud/setup";
 import { MUDProvider } from "./MUDContext";
-import mudConfig from "contracts/mud.config";
+import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { hardhat } from "wagmi/chains";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { transportObserver } from "@latticexyz/common";
+import { fallback, webSocket } from "viem";
+
+const config = createConfig({
+  chains: [hardhat],
+  pollingInterval: 1_000,
+  transports: {
+    [hardhat.id]: transportObserver(fallback([webSocket(), http()])),
+  },
+});
+
+const client = new QueryClient();
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
 const root = ReactDOM.createRoot(rootElement);
 
-// TODO: figure out if we actually want this to be async or if we should render something else in the meantime
-setup().then(async (result) => {
-  root.render(
-    <MUDProvider value={result}>
-      <App />
-    </MUDProvider>,
-  );
-
-  // https://vitejs.dev/guide/env-and-mode.html
-  if (import.meta.env.DEV) {
-    const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
-    mountDevTools({
-      config: mudConfig,
-      publicClient: result.network.publicClient,
-      walletClient: result.network.walletClient,
-      latestBlock$: result.network.latestBlock$,
-      storedBlockLogs$: result.network.storedBlockLogs$,
-      worldAddress: result.network.worldContract.address,
-      worldAbi: result.network.worldContract.abi,
-      write$: result.network.write$,
-      useStore: result.network.useStore,
-    });
-  }
-});
+root.render(
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={client}>
+      <RainbowKitProvider>
+        <MUDProvider>
+          <App />
+        </MUDProvider>
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+);
