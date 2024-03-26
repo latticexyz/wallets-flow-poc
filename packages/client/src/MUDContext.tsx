62ac7329c7cb9d9f36bcfc8f7cc571e26efd758c
alvrs <alvarius@lattice.xyz>
2024-03-26 18:36:12 +0000

    WIP2

--- packages/client/src/MUDContext.tsx
+++ packages/client/src/MUDContext.tsx
@@ -26,56 +26,6 @@ type Props = {
 export const MUDProvider = ({ children }: Props) => {
   const [value, setValue] = useState<MUDContextValue | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const account = useAccount();
-  const store = useMUDStore();
-
-  useEffect(() => {
-    const initSetup = async () => {
-      try {
-        const network: SetupNetworkResult = await setupNetwork();
-        store.setNetwork(network);
-      } catch (error) {
-        console.log("Error setting up MUD client", error);
-      }
-    };
-
-    initSetup();
-  }, []);
-
-  useEffect(() => {
-    const createWallet = async () => {
-      /*
-       * Create an object for communicating with the deployed World.
-       */
-      const networkConfig = await getNetworkConfig();
-      const network = store.network as SetupNetworkResult;
-      const burnerWalletClient = await setupBurnerSigner(network);
-     
-      await initFaucetService(burnerWalletClient, network, networkConfig);
-
-      const worldContract = getContract({
-        address: networkConfig.worldAddress as Hex,
-        abi: IWorldAbi,
-        client: {
-          public: network.publicClient,
-          wallet: burnerWalletClient,
-        },
-      });
-      const systemCalls = createSystemCalls(network, worldContract);
-
-      store.setWalletClient(burnerWalletClient);
-      store.setWorldContract(worldContract);
-      store.setSystemCalls(systemCalls);
-
-      setupDevTools(network, burnerWalletClient, worldContract);
-
-      setLoading(false);
-    };
-
-    if (account?.isConnected && store.network != null && store.worldContract == null && store.systemCalls == null) {
-      createWallet();
-    }
-  }, [account, value, store]);
 
   return (
     <MUDContext.Provider value={value}>
