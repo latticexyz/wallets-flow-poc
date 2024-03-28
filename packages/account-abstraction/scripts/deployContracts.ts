bdbdc12b4e0109db86521e7c4970d6f9ba32b3a6
alvarius <alvarius@lattice.xyz>
2024-03-28 12:19:25 +0000

    feat: add paymaster boilerplate setup (#8)
    * feat: playing around with wallet delegation
    
    * feat: add permissionlessjs boilerplate
    
    * prettierr
    
    * feat: add boilerplate for paymaster
    
    ---------
    
    Co-authored-by: karooolis <hello@karolisram.com>

--- packages/account-abstraction/scripts/deployContracts.ts
+++ packages/account-abstraction/scripts/deployContracts.ts
@@ -1,11 +1,26 @@
+import { deployerClient } from "../src/deploy";
 import { deployEntryPoint } from "../src/deployEntryPoint";
 import { deployEntryPointSimulations } from "../src/deployEntryPointSimulations";
+import { MOCK_PAYMASTER_ADDRESS, deployPaymaster } from "../src/deployPaymaster";
 import { deploySimpleAccountFactory } from "../src/deploySimpleAccountFactory";
+import { getContract, parseAbi, parseEther } from "viem";
 
 export async function deployContracts() {
   await deployEntryPoint();
   await deployEntryPointSimulations();
   await deploySimpleAccountFactory();
+  await deployPaymaster();
+
+  const mockPaymasterContract = getContract({
+    address: MOCK_PAYMASTER_ADDRESS,
+    abi: parseAbi(["function deposit() payable"]),
+    client: deployerClient,
+  });
+
+  const depositHash = await mockPaymasterContract.write.deposit({
+    value: parseEther("50"),
+  });
+  console.log("funded paymaster", depositHash);
 }
 
 deployContracts();
--- packages/client/src/mud/setupSmartAccountClient_verifyingPaymaster.ts
+++ packages/client/src/mud/setupSmartAccountClient_verifyingPaymaster.ts
@@ -1,15 +1,20 @@
-import { http, Hex, PrivateKeyAccount, Address } from "viem";
+import { Hex, Chain } from "viem";
 import { NetworkConfig } from "./getNetworkConfig";
+import { SetupNetworkResult } from "./setupNetwork";
+
+import { http, PrivateKeyAccount, parseEther, Address, createWalletClient } from "viem";
+
 import { createBurnerAccount } from "@latticexyz/common";
-import { writeObserver } from "@latticexyz/common/actions";
+import { transactionQueue, writeObserver } from "@latticexyz/common/actions";
 import { callFrom } from "@latticexyz/world/internal";
 import { ENTRYPOINT_ADDRESS_V07, createSmartAccountClient } from "permissionless";
 import { signerToSimpleSmartAccount } from "permissionless/accounts";
 import { createPimlicoBundlerClient } from "permissionless/clients/pimlico";
 import { call, getTransactionCount } from "viem/actions";
-import { MOCK_PAYMASTER_ADDRESS } from "../../../account-abstraction/src/deployPaymaster";
+
+import { mnemonicToAccount } from "viem/accounts";
+import { foundry } from "viem/chains";
 import { getClientOptions } from "./getClientOptions";
-import { SetupNetworkResult } from "./setupNetwork";
 
 export async function setupSmartAccountClient(
   networkConfig: NetworkConfig,
@@ -26,33 +31,21 @@ export async function setupSmartAccountClient(
     entryPoint: ENTRYPOINT_ADDRESS_V07,
   });
 
+  // 1. Create app signer
   const appSigner = createBurnerAccount(networkConfig.privateKey as Hex) as PrivateKeyAccount;
 
+  // 2. Create smart account for app
   const appSmartAccount = await signerToSimpleSmartAccount(publicClient, {
     entryPoint: ENTRYPOINT_ADDRESS_V07,
     factoryAddress: "0x91E60e0613810449d098b0b5Ec8b51A0FE8c8985",
     signer: appSigner,
   });
 
+  // 3. Create smart account client for app
   const appSmartAccountClient = createSmartAccountClient({
     chain: clientOptions.chain,
     bundlerTransport: http("http://127.0.0.1:4337"),
     middleware: {
-      sponsorUserOperation: async ({ userOperation }) => {
-        const gasEstimates = await pimlicoBundlerClient.estimateUserOperationGas({
-          userOperation: {
-            ...userOperation,
-            paymaster: MOCK_PAYMASTER_ADDRESS,
-            paymasterData: "0x",
-          },
-        });
-
-        return {
-          paymasterData: "0x",
-          paymaster: MOCK_PAYMASTER_ADDRESS,
-          ...gasEstimates,
-        };
-      },
       gasPrice: async () => (await pimlicoBundlerClient.getUserOperationGasPrice()).fast, // use pimlico bundler to get gas prices
     },
     account: appSmartAccount,
@@ -77,6 +70,28 @@ export async function setupSmartAccountClient(
         publicClient,
       }),
     );
+
+  ////////////////////
+  // JUST IN DEV
+  async function seedAccount(to: Address, chain: Chain) {
+    const account = mnemonicToAccount("test test test test test test test test test test test junk", {
+      addressIndex: 3,
+    });
+
+    const walletClient = createWalletClient({
+      account,
+      chain,
+      transport: http("http://localhost:8545"),
+    });
+
+    await walletClient.sendTransaction({
+      to,
+      value: parseEther("5"),
+    });
+  }
+
+  await seedAccount(appSmartAccount.address, foundry);
+  ///////////////////
 
   return appSmartAccountClient;
 }
