862d8b4de8a49904b0fb3c5d558f35e9b8a291aa
alvrs <alvarius@lattice.xyz>
2024-04-03 13:56:21 +0200

    feat: wire up gas-tank paymaster

---
+++ packages/account-abstraction/src/gasTank.ts
@@ -0,0 +1,2 @@
+// Gas tank is locally deployed to this address (deterministic based on deployer address, bytecode, and salt 0x00)
+export const GAS_TANK_ADDRESS = "0x51f1cdd92d92da8ec29d6b680331dbfadcb2681f";
