0e7e41a9bb0e6281a03e962bd31d19fd1e311902
karooolis <hello@karolisram.com>
2024-04-12 22:45:17 +0300

    add gasTankAddress URL param

--- packages/account-abstraction/src/gasTank.ts
+++ packages/account-abstraction/src/gasTank.ts
@@ -1,6 +1,17 @@
 import worlds from "@latticexyz/gas-tank/internal/worlds.json";
-import { Hex } from "viem";
+import { Hex, isHex } from "viem";
 
 export function getGasTankAddress(chainId: number): Hex | undefined {
-  return worlds[chainId]?.address as never;
+  const params = new URLSearchParams(window.location.search);
+  const gasTank = worlds[chainId];
+  const gasTankAddress = params.get("gasTankAddress") || gasTank?.address;
+  if (!gasTankAddress) {
+    throw new Error(`No gas tank address found for chain ${chainId}. Did you run \`mud deploy\`?`);
+  }
+
+  if (!isHex(gasTankAddress)) {
+    throw new Error(`Invalid gas tnk address: ${gasTankAddress}`);
+  }
+
+  return gasTankAddress;
 }
