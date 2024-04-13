3d3a94303e157f9a5068150205d19c0432e562ca
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-13 20:40:53 +0100

    theme based on dark/light mode

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,7 +4,7 @@ import { App } from "./App";
 import { MUDProvider } from "./MUDContext";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { RainbowKitProvider, midnightTheme } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, lightTheme, midnightTheme } from "@rainbow-me/rainbowkit";
 import { MUDAccountKitProvider } from "@latticexyz/account-kit";
 import { networkConfig, queryClient, wagmiConfig } from "./common";
 import { getGasTankAddress } from "account-abstraction/src/gasTank";
@@ -16,7 +16,12 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <WagmiProvider config={wagmiConfig}>
     <QueryClientProvider client={queryClient}>
-      <RainbowKitProvider theme={midnightTheme({ borderRadius: "none" })}>
+      <RainbowKitProvider
+        theme={{
+          lightMode: lightTheme({ borderRadius: "none" }),
+          darkMode: midnightTheme({ borderRadius: "none" }),
+        }}
+      >
         <MUDAccountKitProvider
           config={{
             chainId: networkConfig.chainId,
