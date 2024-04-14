285d2d20d49f6a80dc290e9e2774f82ac0adb84f
alvarius <alvarius@lattice.xyz>
2024-04-14 12:24:11 +0100

    upstream local bundler setup (#14)

--- packages/account-abstraction/src/gasTank.ts
+++
@@ -1,6 +0,0 @@
-import worlds from "@latticexyz/gas-tank/internal/worlds.json";
-import { Hex } from "viem";
-
-export function getGasTankAddress(chainId: number): Hex | undefined {
-  return worlds[chainId]?.address as never;
-}
