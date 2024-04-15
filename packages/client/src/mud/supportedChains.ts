b87d303ccc0982cb2b9c33a9c439287c9ab94e32
karooolis <hello@karolisram.com>
2024-04-15 16:39:35 +0300

    add garnet/holesky contracts to chain config

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -14,9 +14,10 @@
 import { MUDChain, latticeTestnet, mudFoundry } from "@latticexyz/common/chains";
 import { holesky } from "viem/chains";
 
+const sourceId: number = 17000;
 export const garnetHolesky = {
   id: 17069,
-  sourceId: 17000,
+  sourceId,
   name: "Garnet Holesky",
   nativeCurrency: { name: "Holesky Ether", symbol: "ETH", decimals: 18 },
   rpcUrls: {
@@ -33,6 +34,16 @@ export const garnetHolesky = {
     default: {
       name: "Blockscout",
       url: "https://explorer.garnet.qry.live",
+    },
+  },
+  contracts: {
+    gasTank: {
+      address: "0x0cc60b66279359950bf5f257e46e89d1545daf50",
+    },
+    optimismPortal: {
+      [sourceId]: {
+        address: "0x49048044D57e1C92A77f79988d21Fa8fAF74E97e",
+      },
     },
   },
   testnet: true,
